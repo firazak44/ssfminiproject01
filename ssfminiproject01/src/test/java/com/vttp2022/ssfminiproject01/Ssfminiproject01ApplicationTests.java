66d419d07d2e12eddeac959ad4fcb9dd4b2e78e1
Firdaus Razak <razakfirdaus95@gmail.com>
2022-08-15 14:29:18 +0800

    first com

---
+++ ssfminiproject01/src/test/java/com/vttp2022/ssfminiproject01/Ssfminiproject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vttp2022.ssfminiproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssfminiproject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
